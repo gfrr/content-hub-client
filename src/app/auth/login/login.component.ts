713b74061e96674af047e99b0a9af8f74f50fcd7
Phillip Kickum <filkckm@Phillips-MacBook-Pro.local>
2017-05-31 14:54:45 +0200

    index background login signup

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -27,7 +27,7 @@ export class LoginComponent implements OnInit {
   login() {
     this.session.login(this.user)
 				        .subscribe(result => {
-				            if (result === true) this.router.navigate(['/index']);
+				            if (result === true) this.router.navigate(['/content']);
 			         			else this.error = 'Username or password is incorrect';
 				        });
   }
